cc0454ec5deec2cda9864fba06a6170f8a8e8186
dword1511 <zhangchi866@gmail.com>
2017-06-21 13:42:53 -0500

    fix thermal.sh

--- thermal.sh
+++ thermal.sh
@@ -2,18 +2,21 @@
 
 for zone in /sys/class/thermal/thermal_zone*
 do
-  NUM=`basename $zone`
+  NUM=`basename "$zone"`
   NUM=${NUM#thermal_zone}
-  TYPE=`cat $zone/type`
-  TEMP=`cat $zone/temp`
+  TYPE=`cat "$zone/type"`
+  TEMP=`cat "$zone/temp"`
   TRIP=""
-  for trip in $zone/trip_point_*_temp
-  do
-    TNUM=`basename $trip | cut -d '_' -f 3`
-    TTYPE=`cat $zone/trip_point_${TNUM}_type`
-    TTEMP=`cat $trip`
-    TTEMP=`awk "BEGIN {printf(\"%.1f°C\", $TTEMP / 1000)}"`
-    TRIP="$TRIP, $TTYPE = $TTEMP"
-  done
+  if test -e `echo "$zone"/trip_point_*_temp | cut -d ' ' -f 1`
+  then
+    for trip in "$zone"/trip_point_*_temp
+    do
+      TNUM=`basename $trip | cut -d '_' -f 3`
+      TTYPE=`cat "$zone/trip_point_${TNUM}_type"`
+      TTEMP=`cat "$trip"`
+      TTEMP=`awk "BEGIN {printf(\"%.1f°C\", $TTEMP / 1000)}"`
+      TRIP="$TRIP, $TTYPE = $TTEMP"
+    done
+  fi
   awk "BEGIN {printf(\"zone%d: %5.1f°C ($TYPE$TRIP)\n\", $NUM, $TEMP / 1000)}"
 done
